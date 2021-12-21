4d9da098274619ce4be826e8f584b0da557c3907
JunyanSu <junyan.su@my.cityu.edu.hk>
2021-12-21 13:21:43 +0800

    fix notations in .ino file

--- examples/arduino_example/arduino_example.ino
+++ examples/arduino_example/arduino_example.ino
@@ -8,12 +8,13 @@ void setup() {
   Serial.println("begin");
 }
 
+#define x_size (N*(nx+nu)+nx)
+
 void loop() {
     static double x0[] = {10,0};
     static double u0[nu];
-    static double yin[y_size];
     static double lam_in[lam_size];
-    static double yout[y_size];
+    static double stack_x[x_size];
     static double lam_out[lam_size];
     static size_t max_iter = 3;
     static double tol = 1e-3;
@@ -21,7 +22,7 @@ void loop() {
     pempc_aAx('n',1,A,x0,nx,nx,x0);
     pempc_aAxpy('n',1,B,u0,nx,nu,x0);
     time_ = micros();
-    pempc_get_control(x0,max_iter,tol,yout,lam_out,yout,lam_out,u0);
+    pempc_get_control(x0,max_iter,tol,stack_x,lam_out,stack_x,lam_out,u0);
     time_ = micros() -time_;
     Serial.println(time_/1000.0f);
     Serial.println(u0[0]);
