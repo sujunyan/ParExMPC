e004e70b0f0632f49858aeeb8fae3faff8cc80d9
Juraj Oravec <juraj.oravec@stuba.sk>
2023-10-26 15:12:36 +0200

    Code: Update: Minor update in MAIN_TOY_EXAMPLE.M

--- main_toy_example.m
+++ main_toy_example.m
@@ -1,34 +1,42 @@
+% initialization ------
 mpt_init;
-addpath(genpath("@peMPC")); 
+addpath(genpath("@peMPC"));
 addpath("examples");
 
 % define the problem ------
-  Q = [10,0;0,10]; 
-  R = 1;
-  A = [0.7115,-0.4345; 0.4345, 0.8853];
-  B = [0.2173;0.0573];
-  [~,P] = dlqr(A,B,Q,R);
-  umin = -5; umax = 5;
-  N = 3;
-  x0 = [10;0];
-  
-  % use the ParExMPC interface ------
-  mpc0 = peMPC(A,B,Q,R,P,'umin',umin,'umax',umax,'N',N);
+A = [0.7115,-0.4345; 0.4345, 0.8853];
+B = [0.2173;0.0573];
+Q = [10,0;0,10];
+R = 1;
+[~,P] = dlqr(A,B,Q,R);
+umin = -5; umax = 5;
+N = 3;
+x0 = [10;0];
 
-  mpc0 = mpc0.build; 
-  maxiter = 5;
-  % start of MPC simulation ------
-  nsim = 100;
-  [nx,nu] = size(B);
-  tol = 1e-4;               % the tolerance in the MPC iteration
-  for i = 1:nsim
-    if (i == 1)             % solve the first problem with large maximum iteration
-      lam = zeros((N+1)*nx,1);
-      z = zeros(N*(nx+nu) + nx ,1);
-      [z,lam,u0] = peMPC_controller_mex(x0,100,tol,z,lam);
+% use the ParExMPC interface ------
+% call help peMPC to see available optional parameters.
+mpc0 = peMPC(A,B,Q,R,P,'umin',umin,'umax',umax,'N',N);
+mpc0 = mpc0.build;
+
+% start of MPC simulation ------
+maxiter = 5;
+nsim = 100;
+[nx,nu] = size(B);
+% the tolerance in the MPC iteration
+tol = 1e-4;
+X = [];
+U = [];
+for k = 1:nsim
+    % solve the first problem with a large maximum iteration
+    if (k == 1)
+        lam = zeros((N+1)*nx,1);
+        z = zeros(N*(nx+nu) + nx ,1);
+        [z,lam,u0] = peMPC_controller_mex(x0,100,tol,z,lam);
     else
-      [z,lam,u0] = peMPC_controller_mex(x0,maxiter,tol,z,lam);
+        [z,lam,u0] = peMPC_controller_mex(x0,maxiter,tol,z,lam);
     end
-    x0 = A*x0+B*u0;         % simulate one step of the system dynamics
-  end
-  
+    % simulate one step of the system dynamics
+    x0 = A*x0+B*u0;
+    X = [X,x0];
+    U = [U,u0];
+end
